1f8c7cb9fbace7929f5117c40824c4d72aa426fe
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-08-03 21:01:23 -0400

    Adding sample tests

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(a, b) {
+    if (b == 0) {
+      throw new Error('Cannot divide by 0');
+    }
+  
+    return a / b;
+  }
+
+  module.exports = divide;
---
+++ test/divide.test.js
@@ -0,0 +1,33 @@
+const divide = require('../index');
+const chai = require('chai');
+const expect = chai.expect;
+//const expect = require('chai').expect;
+
+describe('Divide function', () => {
+    it('should divide positive integers correctly', () => {
+        // define inputs
+        const a = 8;
+        const b = 4;
+        const expectedAnswer = 2;
+      
+        // invoke the function
+        const actualAnswer = divide(a, b);
+      
+        // assert that expected === actual
+        expect(actualAnswer).to.equal(expectedAnswer);
+      });
+
+      it('should throw an error when divided by zero', () => {
+        // define inputs
+        const a = 8, b = 0;
+      
+        // set up the function call
+        const fn = () => {
+          divide(a, b)
+        };
+      
+        // assert that exception is thrown
+        expect(fn).to.throw();
+      });
+});
+
